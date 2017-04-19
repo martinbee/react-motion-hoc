c222ef0f14c235a7505bb3647c3e7bdc3db11344
Martin Bee <martbee88@gmail.com>
2017-04-19 20:13:16 +0000

    Add default style and style based on animation type passed in. Update readme.

--- src/components/MotionHOCWithCurry.js
+++ src/components/MotionHOCWithCurry.js
@@ -2,12 +2,20 @@ import React from 'react';
 import _ from 'lodash';
 import {Motion, spring} from 'react-motion';
 
+function getStyleSetup(animationType) {
+  if (animationType === "Grow") {
+    return { default: { height: 0, width: 0 }, style: { height: spring(50), width: spring(50) }};
+  } else if (animationType === "FadeIn") {
+    return { default: { opacity: 0 }, style: { opacity: spring(1) }};
+  }
+}
+
 function MotionHOC(animationType, WrappedElement) {
-  console.log(animationType);
+  const styleSetup = getStyleSetup(animationType);
 
   return function(props) {
     return (
-      <Motion defaultStyle={{ x: 0 }} style={{ x: spring(10) }}>
+      <Motion defaultStyle={styleSetup.default} style={styleSetup.style}>
         {value => <WrappedElement {...props} {...value} />}
       </Motion>
     );
