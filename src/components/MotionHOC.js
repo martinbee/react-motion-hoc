508326f954fb99e7c05d7c732981b15d1d01b450
Bee <Martin.Bee@aig.com>
2017-04-19 11:47:36 -0500

    Get simple demo working.

--- src/components/MotionHOC.js
+++ src/components/MotionHOC.js
@@ -2,13 +2,12 @@ import React from 'react';
 import _ from 'lodash';
 import {Motion, spring} from 'react-motion';
 
-
 export default function MotionHOC(WrappedElement) {
-  console.log(WrappedElement);
-
-  return (
-    <Motion defaultStyle={{ x: 0 }} style={{ x: spring(10) }}>
-      {value => <WrappedElement {..._.merge(this.props, { text: value.x  })} />}
-    </Motion>
-  );
+  return function(props) {
+		return (
+			<Motion defaultStyle={{ x: 0 }} style={{ x: spring(10) }}>
+				{value => <WrappedElement text={value.x} />}
+			</Motion>
+		);
+	}
 }
